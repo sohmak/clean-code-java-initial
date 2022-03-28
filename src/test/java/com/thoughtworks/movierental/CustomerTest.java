20e873c7df421d4b54e518254b283cd18ccb089c
robmakaremi <rob.m@thoughtworks.com>
2022-03-28 17:07:12 +1000

    Adding Test

--- src/test/java/com/thoughtworks/movierental/CustomerTest.java
+++ src/test/java/com/thoughtworks/movierental/CustomerTest.java
@@ -12,4 +12,24 @@ public class CustomerTest {
         Assert.assertEquals(expected, 1 + 2);
     }
 
+    @Test
+    public void statementTest() {
+        Customer jimmy = new Customer("Jimmy");
+        jimmy.addRental(new Rental(new Movie("Inception", 0), 2));
+        jimmy.addRental(new Rental(new Movie("Whiplash", 1), 2));
+        jimmy.addRental(new Rental(new Movie("Titanic", 1), 3));
+
+        String expected = jimmy.statement();
+
+        Assert.assertEquals(expected, "Rental Record for Jimmy\n" +
+                "\tInception\t2.0\n" +
+                "\tWhiplash\t6.0\n" +
+                "\tTitanic\t9.0\n" +
+                "Amount owed is 17.0\n" +
+                "You earned 5 frequent renter points");
+
+
+    }
+
+
 }
